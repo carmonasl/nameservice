575d4e68bcc4a6623f59099339c5acf9f739e080
carmonasl <alberto.carmona.sanchez@gmail.com>
2022-04-29 22:59:48 +0200

    set and delete messages scaffolded

--- x/nameservice/module_simulation.go
+++ x/nameservice/module_simulation.go
@@ -28,6 +28,14 @@ const (
 	// TODO: Determine the simulation weight value
 	defaultWeightMsgBuyName int = 100
 
+	opWeightMsgSetName = "op_weight_msg_set_name"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgSetName int = 100
+
+	opWeightMsgDeleteName = "op_weight_msg_delete_name"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgDeleteName int = 100
+
 	// this line is used by starport scaffolding # simapp/module/const
 )
 
@@ -71,6 +79,28 @@ func (am AppModule) WeightedOperations(simState module.SimulationState) []simtyp
 	operations = append(operations, simulation.NewWeightedOperation(
 		weightMsgBuyName,
 		nameservicesimulation.SimulateMsgBuyName(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
+	var weightMsgSetName int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgSetName, &weightMsgSetName, nil,
+		func(_ *rand.Rand) {
+			weightMsgSetName = defaultWeightMsgSetName
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgSetName,
+		nameservicesimulation.SimulateMsgSetName(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
+	var weightMsgDeleteName int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgDeleteName, &weightMsgDeleteName, nil,
+		func(_ *rand.Rand) {
+			weightMsgDeleteName = defaultWeightMsgDeleteName
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgDeleteName,
+		nameservicesimulation.SimulateMsgDeleteName(am.accountKeeper, am.bankKeeper, am.keeper),
 	))
 
 	// this line is used by starport scaffolding # simapp/module/operation
